77eaa9394153359ae2148450635659f103fe25e8
Harkirat155 <harkirat.hira@gmail.com>
2020-10-20 18:19:41 +0530

    pypi release, [WIP] deployment ci

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='pystatdp',
-    version='0.0.2',
+    version='0.1.0',
     description='Counterexample Detection Using Statistical Methods for Incorrect Differential-Privacy Algorithms.',
     long_description=long_description,
     long_description_content_type="text/markdown",
